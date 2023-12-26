82d4b1aa0c45b81c35def97686eb3a413ae1f487
Samriddh Saxena <samriddhsaxena@gmail.com>
2023-12-26 19:56:30 +0530

    running

---
+++ Exercises/Example Questions 1/examples.py
@@ -0,0 +1,15 @@
+# Write a Python program to print "Hello, World!"
+
+print("Hello World!")
+
+# Calculate the sum of two numbers entered by the user
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print(a+b)
+
+# Convert temperature from Celsius to Fahrenheit.
+
+celcius = int(input("Enter Celcius Temperature: "))
+farenheit = (celcius*9)/5 + 32
+print(farenheit)
