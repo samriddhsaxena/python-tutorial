f46861c9f3c6874788296f90b745048cbf22ab21
Samriddh Saxena <samriddhsaxena@gmail.com>
2023-12-27 00:40:24 +0530

    done

--- Exercises/Example Questions 1/practice questions.py
+++ Exercises/Example Questions 1/practice questions.py
@@ -17,5 +17,34 @@ if(a%2==0):
 else:
     print("The number is odd.")
 
-# Given a list of numbers, find the maximum and minimum values
-    
+# Calculate the compound interest for a given principal amount, interest rate, and time period
+prinAmt = 50000
+rate = 10
+time = 5
+amt = prinAmt*((1+(rate/100))**time)
+compoundInt = amt - prinAmt
+print(compoundInt)
+
+# Write a program that converts a given number of days into years, weeks, and days
+totalDays = 747
+years = int(totalDays/365)
+remYears = int(totalDays%365)
+weeks = int(remYears/7)
+days = int(remYears%7)
+print (years,"years ",weeks,"weeks ",days,"days ")
+
+# Given a list of integers, find the sum of all positive numbers
+listOfInt = [1,2,3,4,5]
+sum = 0
+for i in range(5):
+    sum+=listOfInt[i]
+print(sum)
+
+# Implement a program that swaps the values of two variables.
+a = 2
+b = 4
+print(a,b)
+temp = a
+a = b
+b = temp
+print(a,b)
