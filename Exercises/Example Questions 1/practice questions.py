f311a412b4387fa0514d617df3903fd5a4a4dd71
Samriddh Saxena <samriddhsaxena@gmail.com>
2023-12-26 20:38:21 +0530

    running

---
+++ Exercises/Example Questions 1/practice questions.py
@@ -0,0 +1,21 @@
+# Write a Python program to calculate the area of a rectangle given its length and width
+length = 20
+breadth = 30
+area = length*breadth
+print(area)
+
+# Create a program that takes a user's name and age as input and prints a greeting message
+# name = input("Enter your name: ")
+# age = (input("Enter your age: "))
+# greeting = "Good Morning "+ name+". Your age is "+age+"."
+# print(greeting)
+
+# Write a program to check if a number is even or odd
+a = int(input("Enter the number to be checked: "))
+if(a%2==0):
+    print("The number is even.")
+else:
+    print("The number is odd.")
+
+# Given a list of numbers, find the maximum and minimum values
+    
