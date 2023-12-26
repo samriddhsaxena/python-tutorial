b98e8bee5713bc4aec8ba5c3a5fefc60910eb10b
Samriddh Saxena <samriddhsaxena@gmail.com>
2023-12-26 19:39:15 +0530

    done

---
+++ Chapter 1/02_pr_01_twinkle.py
@@ -0,0 +1,3 @@
+print('''Twinkle Twinkle little star,how I wonder what you are!
+Up above the world so high
+Like a diamond in the sky''')
---
+++ Chapter 1/03_pr_02_module_usage.py
@@ -0,0 +1,2 @@
+from playsound import playsound
+playsound('C:\\Users\\samri\\OneDrive\\Documents\\Python Tutorial\\Chapter 1\\play.mp3')
